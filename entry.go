6d3521731e3986bd04c40ae00247d749821b2de1
dongchaofeng <dongjialong2006@163.com>
2018-09-10 17:10:08 +0800

    :bug:fix(v0.12): 修改日志打印的逻辑
    Signed-off-by: dongchaofeng <dongjialong2006@163.com>

--- entry.go
+++ entry.go
@@ -14,114 +14,122 @@ type Entry struct {
 }
 
 func (e Entry) Debug(args ...interface{}) {
-	_, file, line, ok := runtime.Caller(1)
-	if !ok || !e.caller {
+	if !e.caller {
 		e.log.Debug(args...)
 		return
 	}
 
+	_, file, line, _ := runtime.Caller(1)
 	e.log.WithFields(logrus.Fields{"file": file, "line": line}).Debug(args...)
 }
 
 func (e Entry) Debugf(format string, args ...interface{}) {
-	_, file, line, ok := runtime.Caller(1)
-	if !ok || !e.caller {
+	if !e.caller {
 		e.log.Debugf(format, args...)
 		return
 	}
+
+	_, file, line, _ := runtime.Caller(1)
 	e.log.WithFields(logrus.Fields{"file": file, "line": line}).Debugf(format, args...)
 }
 
 func (e Entry) Info(args ...interface{}) {
-	_, file, line, ok := runtime.Caller(1)
-	if !ok || !e.caller {
+	if !e.caller {
 		e.log.Info(args...)
 		return
 	}
+
+	_, file, line, _ := runtime.Caller(1)
 	e.log.WithFields(logrus.Fields{"file": file, "line": line}).Info(args...)
 }
 
 func (e Entry) Infof(format string, args ...interface{}) {
-	_, file, line, ok := runtime.Caller(1)
-	if !ok || !e.caller {
+	if !e.caller {
 		e.log.Infof(format, args...)
 		return
 	}
+
+	_, file, line, _ := runtime.Caller(1)
 	e.log.WithFields(logrus.Fields{"file": file, "line": line}).Infof(format, args...)
 }
 
 func (e Entry) Error(args ...interface{}) {
-	_, file, line, ok := runtime.Caller(1)
-	if !ok || !e.caller {
+	if !e.caller {
 		e.log.Error(args...)
 		return
 	}
+
+	_, file, line, _ := runtime.Caller(1)
 	e.log.WithFields(logrus.Fields{"file": file, "line": line}).Error(args...)
 }
 
 func (e Entry) Errorf(format string, args ...interface{}) {
-	_, file, line, ok := runtime.Caller(1)
-	if !ok || !e.caller {
+	if !e.caller {
 		e.log.Errorf(format, args...)
 		return
 	}
+
+	_, file, line, _ := runtime.Caller(1)
 	e.log.WithFields(logrus.Fields{"file": file, "line": line}).Errorf(format, args...)
 }
 
 func (e Entry) Warn(args ...interface{}) {
-	_, file, line, ok := runtime.Caller(1)
-	if !ok || !e.caller {
+	if !e.caller {
 		e.log.Warn(args...)
 		return
 	}
+
+	_, file, line, _ := runtime.Caller(1)
 	e.log.WithFields(logrus.Fields{"file": file, "line": line}).Warn(args...)
 }
 
 func (e Entry) Warnf(format string, args ...interface{}) {
-	_, file, line, ok := runtime.Caller(1)
-	if !ok || !e.caller {
+	if !e.caller {
 		e.log.Warnf(format, args...)
 		return
 	}
+
+	_, file, line, _ := runtime.Caller(1)
 	e.log.WithFields(logrus.Fields{"file": file, "line": line}).Warnf(format, args...)
 }
 
 func (e Entry) Fatal(args ...interface{}) {
-	_, file, line, ok := runtime.Caller(1)
-	if !ok || !e.caller {
+	if !e.caller {
 		e.log.Fatal(args...)
 		return
 	}
+
+	_, file, line, _ := runtime.Caller(1)
 	e.log.WithFields(logrus.Fields{"file": file, "line": line}).Fatal(args...)
 }
 
 func (e Entry) Fatalf(format string, args ...interface{}) {
-	_, file, line, ok := runtime.Caller(1)
-	if !ok || !e.caller {
+	if !e.caller {
 		e.log.Fatalf(format, args...)
 		return
 	}
 
+	_, file, line, _ := runtime.Caller(1)
 	e.log.WithFields(logrus.Fields{"file": file, "line": line}).Fatalf(format, args...)
 }
 
 func (e Entry) Panic(args ...interface{}) {
-	_, file, line, ok := runtime.Caller(1)
-	if !ok || !e.caller {
+	if !e.caller {
 		e.log.Panic(args...)
 		return
 	}
 
+	_, file, line, _ := runtime.Caller(1)
 	e.log.WithFields(logrus.Fields{"file": file, "line": line}).Panic(args...)
 }
 
 func (e Entry) Panicf(format string, args ...interface{}) {
-	_, file, line, ok := runtime.Caller(1)
-	if !ok || !e.caller {
+	if !e.caller {
 		e.log.Panicf(format, args...)
 		return
 	}
 
+	_, file, line, _ := runtime.Caller(1)
 	e.log.WithFields(logrus.Fields{"file": file, "line": line}).Panicf(format, args...)
 }
 
